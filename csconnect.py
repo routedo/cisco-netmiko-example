086c3a328b6d11e96db0bd79ba330debdae51416
route <routedo>
2018-03-06 11:20:54 -0500

    pep8 cleanup

--- csconnect.py
+++ csconnect.py
@@ -1,19 +1,27 @@
-from netmiko import ConnectHandler
+"""
+Query the ARP table and show running-config on a Cisco network device.
+"""
+
 import re
 import sys
-
-## dev = Cisco device connection 
-## ip_find = IP address
-## return = Returns MAC address for specified IP address
+from netmiko import ConnectHandler
 
 def cisco_arp_to_mac(dev, ip_find):
+    """
+    This function queries the ARP table and returns a mac address.
+
+    dev = Cisco device connection
+    ip_find = IP address
+    return = Returns MAC address for specified IP address
+    """
+
     try:
 
         ## Regex term
         search_term = '\w*\s*(\d+\.\d+.\d+.\d+)\s*(-|\d+)\s*(.{14})\s*ARPA\s*(.*)'
 
         ## Run show arp command
-        output = dev.send_command("show arp  "+ ip_find)
+        output = dev.send_command('show arp ' + ip_find)
         output = output.split('\n')
 
         ## Search for MAC address
@@ -22,27 +30,30 @@ def cisco_arp_to_mac(dev, ip_find):
             if result:
                 mac_has = result.group(3).split('.')
                 mac_has = ''.join(mac_has)
-                mac_has = ':'.join(format(s, '02x') for s in bytes.fromhex(mac_has))    
+                mac_has = ':'.join(format(s, '02x') for s in bytes.fromhex(mac_has))
             else:
                 mac_has = 'MAC not found'
-       
-        return(mac_has)
-        
-    except Exception as err: 
+
+        return mac_has
+
+    except Exception as err:
         print(err)
         sys.exit(1)
 
-## dev = Cisco device connection 
-## return = Returns running-config      
-
 def cisco_get_config(dev):
+    """
+    This function runs the show running-config command.
+
+    dev = Cisco device connection
+    return = Returns running-config
+    """
+
     try:
-        
         ## Run show running-config
-        output = dev.send_command("show running-config")
+        output = dev.send_command('show running-config')
 
-        return(output)
+        return output
 
-    except Exception as err: 
+    except Exception as err:
         print(err)
         sys.exit(1)
--- netmikoexample.py
+++ netmikoexample.py
@@ -1,48 +1,51 @@
+"""
+This example shows how to use netmiko to query the ARP table and running config of a cisco device.
+"""
+
+import sys
 from getpass import getpass
 from netmiko import ConnectHandler
-import sys
 from csconnect import cisco_arp_to_mac
 from csconnect import cisco_get_config
 
 def main():
+    """ Example """
     try:
-    
         ## Login credentials
         username = input("Cisco username: ")
         passwd = getpass('Enter Cisco Password: ')
-        
+
         ## Network device IP address
         dev_ip = '10.10.10.11'
-        
+
         ## Specify IP address to find
         arp_to_find = '1.1.1.1'
-        
+
         ## Location to store config
         config_location = 'config/cisco.conf'
-        
+
         ## Create ssh connection to Cisco device
-        dev_connect = ConnectHandler(device_type='cisco_ios', ip=dev_ip, username=username, password=passwd) 
+        dev_connect = ConnectHandler(device_type='cisco_ios', ip=dev_ip, username=username, password=passwd)
 
         ## Find MAC address of IP address
         mac_addr = cisco_arp_to_mac(dev_connect, arp_to_find)
-        
+
         ## Pull running config from network device
         config_save = cisco_get_config(dev_connect)
-        
+
         ## Write running config to file
-        with open(config_location,"w") as f:
+        with open(config_location, 'w') as f:
             for line in config_save:
                 f.write(line)
-        
+
         print('\n MAC address for IP : ' + arp_to_find + ' is ' + mac_addr)
-        
+
         ## Disconnect from device
         dev_connect.disconnect()
-        
 
-    except Exception as Err:
-        print(Err)
+    except Exception as err:
+        print(err)
         sys.exit(1)
 
-if __name__== "__main__":
+if __name__ == '__main__':
     main()
